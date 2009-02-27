8c69bfb737f7a6ed88637a3062d1d44477af68cd
maedana <maeda.na@gmail.com>
2009-02-27 14:46:25 +0900

    hamlはproductionでは不要になるように

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,8 +1,10 @@
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  require 'haml' # From gem
-end
+unless Rails.env == 'production'
+  begin
+    require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
+  rescue LoadError
+    require 'haml' # From gem
+  end
 
-# Load Haml and Sass
-Haml.init_rails(binding)
+  # Load Haml and Sass
+  Haml.init_rails(binding)
+end
